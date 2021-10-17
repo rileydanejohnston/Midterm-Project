30b97bbdf1c380a4d66834b484bfba4a6b2aa748
Riley Johnston <rileydanejohnston@gmail.com>
2021-10-16 22:29:35 -0500

    add private variables, create constructor/destructor, set functions for number/name/days/units

--- Midterm/Course.cpp
+++ Midterm/Course.cpp
@@ -3,3 +3,34 @@
 // Midterm
 
 #include "Course.h"
+
+Course::Course(string num, string title, string meetDays, double numUnits, const Date &startDt, const Date &endDt, const Time &startTm, const Time &endTm)
+:startDate(startDt), endDate(endDt), startTime(startTm), endTime(endTm)
+{
+    setNumber(num).setName(title).setDays(meetDays).setUnits(numUnits);
+}
+Course::~Course()
+{
+    cout << "The course has been deleted." << endl;
+}
+
+Course& Course::setNumber(string num)
+{
+    number = num;
+    return *this;
+}
+Course& Course::setName(string title)
+{
+    name = title;
+    return *this;
+}
+Course& Course::setDays(string meetDays)
+{
+    days = meetDays;
+    return *this;
+}
+Course& Course::setUnits(double numUnits)
+{
+    units = numUnits;
+    return *this;
+}
--- Midterm/Course.h
+++ Midterm/Course.h
@@ -4,12 +4,31 @@
 
 #ifndef COURSE_H
 #define COURSE_H
+#include <iostream>
+#include <string>
+#include "Date.h"
+#include "Time.h"
+using namespace std;
 
 class Course
 {
 private:
-    
+    string number;
+    string name;
+    string days;
+    double units;
+    Date startDate;
+    Date endDate;
+    Time startTime;
+    Time endTime;
 public:
+    Course(string, string, string, double, const Date&, const Date&, const Time&, const Time&);
+    ~Course();
+    
+    Course& setNumber(string);
+    Course& setName(string);
+    Course& setDays(string);
+    Course& setUnits(double);
     
 };
 
