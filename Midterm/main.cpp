d1348e307936e750b4e896b1ce8de29e5313cccb
Riley Johnston <rileydanejohnston@gmail.com>
2021-10-25 13:25:41 -0500

    add while loop menu setup for Main.cpp

--- Midterm/main.cpp
+++ Midterm/main.cpp
@@ -16,9 +16,11 @@ int main()
     // get semester name
     // get sem start date
     // get sem end date
+    char menuSelect;
     string name;
     int maxClasses;
     Semester userSem;
+    
     
     cout << "Please enter your name: ";
     getline(cin, name);
@@ -29,10 +31,46 @@ int main()
     
     cout << "Please enter the maximum number of classes you can take: ";
     cin >> maxClasses;
-    
     CourseSchedule userSchedule(name, userSem, maxClasses);
+     
+    do
+    {
+        cout << endl;
+        cout << "COURSE ENTRY MENU FOR: " << userSem << endl;
+        cout << "-------------------------------------------------------" << endl;
+        cout << "1) Enter a new course" << endl;
+        cout << "2) Remove a course" << endl;
+        cout << "3) Print a Semester Schedule" << endl;
+        cout << "q) Quit the program" << endl;
+        cout << "Your selection: ";
+        cin >> menuSelect;
+        cout << endl;
+        
+        
+        if (menuSelect == '1')
+        {
+            cout << "You selected " << menuSelect << endl;
+        }
+        else if (menuSelect == '2')
+        {
+            cout << "You selected " << menuSelect << endl;
+        }
+        else if (menuSelect == '3')
+        {
+            cout << userSchedule;
+        }
+        else if (menuSelect == 'q' || menuSelect == 'Q')
+        {
+            cout << "You chose to quit. Thank you for using the program!" << endl;
+        }
+        else
+        {
+            cout << "Invalid selection. Please try again." << endl;
+        }
+        
+    }
+    while (menuSelect != 'q' && menuSelect != 'Q');
     
-    cout << userSchedule << endl;
     
     
     return 0;
